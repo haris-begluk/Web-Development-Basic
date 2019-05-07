9aa18f2f036c86da32862f374ebbecb9803dab87
haris-begluk <harisbegluk@gmail.com>
2019-05-07 20:22:34 +0200

    Responsive navigation bar

---
+++ script.js
@@ -0,0 +1,8 @@
+function OpenClose() {
+    var x = document.getElementById("topNav");
+    if (x.className === "navbar") {
+        x.className += " responsive";
+    } else {
+        x.className = "navbar";
+    }
+}
