1db673eb9b959a9e62701627328e9c48001ce66c
Haris-Begluk <haris.begluk@edu.fit.ba>
2020-03-31 00:17:07 +0200

    RXJs practice

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
-function OpenClose() {
-    var x = document.getElementById("topNav");
-    if (x.className === "navbar") {
-        x.className += " responsive";
-    } else {
-        x.className = "navbar";
-    }
+const button = document.querySelector("button");
+const div = document.querySelector("div");
+
+const setText = (text) => {
+  div.textContent = text
 }
+
+const checkAuth = cb => {
+  setText('Checking Auth...')
+  setTimeout(() => {
+    cb(true);
+  }, 2000);
+};
+
+const fetchUser = cb => {
+  setText('Fetching User...')
+  setTimeout(() => {
+    cb({ name: "Max" });
+  }, 2000);
+};
+
+button.addEventListener("click", () => {
+  checkAuth(auth => { 
+    if (auth) {
+      fetchUser(user => { 
+        setText(user.name)
+      });
+    }
+  });
+});
