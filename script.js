00051177082681601c3af2f387c1437daf5e9115
Haris-Begluk <haris.begluk@edu.fit.ba>
2020-03-31 10:57:55 +0200

    Using Promises

--- script.js
+++ script.js
@@ -1,30 +1,37 @@
+//Callbacks
 const button = document.querySelector("button");
 const div = document.querySelector("div");
 
-const setText = (text) => {
-  div.textContent = text
-}
-
-const checkAuth = cb => {
-  setText('Checking Auth...')
-  setTimeout(() => {
-    cb(true);
-  }, 2000);
+const setText = text => {
+  div.textContent = text;
 };
 
-const fetchUser = cb => {
-  setText('Fetching User...')
-  setTimeout(() => {
-    cb({ name: "Max" });
-  }, 2000);
+const checkAuth = () => {
+  return new Promise((resolve, reject) => {
+    setText("Checking Auth...");
+    setTimeout(() => {
+      resolve(true);
+    }, 2000);
+  });
+};
+
+const fetchUser = () => {
+  return new Promise((resolve, reject) => {
+    setText("Fetching User...");
+    setTimeout(() => {
+      resolve({ name: "Max" });
+    }, 2000);
+  });
 };
 
 button.addEventListener("click", () => {
-  checkAuth(auth => { 
-    if (auth) {
-      fetchUser(user => { 
-        setText(user.name)
-      });
-    }
-  });
+  checkAuth()
+    .then(isAuth => {
+      if (isAuth) {
+        return fetchUser();
+      }
+    })
+    .then(user => {
+      setText("User fetched: " +user.name);
+    });
 });
